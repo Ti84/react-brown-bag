930d87136f71f1e74c278f32f0c703da850a6736
Shane4666 <sc897@nau.edu>
2020-09-15 23:00:26 -0700

    oops

--- src/slides/overview.js
+++ src/slides/overview.js
@@ -11,7 +11,7 @@ const Overview = () => (
   <Slide bgColor="primary">
     <Heading size={3} textColor="secondary">Overview</Heading>
     <List textColor="tertiary">
-      <ListItem margin="10px 0 0 0">\</ListItem>
+      <ListItem margin="10px 0 0 0">Current popular frontend "frameworks"</ListItem>
       {/* Javascript, Html, Css, etc */}
       <ListItem margin="10px 0 0 0">What you need to know before learning React</ListItem>
       <ListItem margin="10px 0 0 0">What is React?</ListItem>
